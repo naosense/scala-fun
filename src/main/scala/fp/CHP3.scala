02675784b5efd49aded04cc1f78637c477552306
wocanmei <pingao777@gmail.com>
2020-06-27 21:09:10 +0800

    ex3.5

--- src/main/scala/fp/CHP3.scala
+++ src/main/scala/fp/CHP3.scala
@@ -1,5 +1,7 @@
 package fp
 
+import scala.annotation.tailrec
+
 object CHP3 {
 
   sealed trait List[+A]
@@ -35,10 +37,45 @@ object CHP3 {
     case _ => 101
   }
 
+  // ex3.2
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, xs) => xs
+  }
 
+  // ex3.3
+  def setHead[A](l: List[A], x: A): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, xs) => Cons(x, xs)
+  }
+
+  // ex3.4
+  @tailrec
+  def drop[A](l: List[A], n: Int): List[A] = {
+    n match {
+      case 0 => l
+      case _ =>
+        l match {
+          case Nil => Nil
+          case Cons(_, xs) => drop(xs, n - 1)
+        }
+    }
+  }
+
+  // ex3.5
+  @tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(x, xs) if f(x) => dropWhile(xs, f)
+      case _ => l
+    }
+  }
 
   def main(args: Array[String]): Unit = {
     println(List("a", "b"))
     println(x)
+    println(drop(List(1, 2, 3, 4, 5), 2))
+    println(dropWhile(List(1, 2, 3, 4, 5), (x: Int) => x < 3))
   }
 }
