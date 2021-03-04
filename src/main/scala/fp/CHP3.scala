edb242d65b8cf5550939ed3da5f8071e6ea8ea16
pingao <liupingao@jd.com>
2021-03-04 11:50:27 +0800

    3.18

--- src/main/scala/fp/CHP3.scala
+++ src/main/scala/fp/CHP3.scala
@@ -1,10 +1,29 @@
 package fp
 
+import fp.CHP3.List.foldRight
+
 import scala.annotation.tailrec
 
 object CHP3 {
 
-  sealed trait List[+A]
+  sealed trait List[+A] {
+    override def toString: String = {
+      @tailrec
+      def go(list: List[A], sb: StringBuilder): Unit = {
+        list match {
+          case Nil => sb.deleteCharAt(sb.length() - 1); sb.append(']')
+          case Cons(head, tail) => sb.append(head).append(','); go(tail, sb)
+        }
+      }
+
+      if (this eq Nil) "[]"
+      else {
+        val sb = new StringBuilder("[")
+        go(this, sb)
+        sb.toString()
+      }
+    }
+  }
   case object Nil extends List[Nothing]
   case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
@@ -24,6 +43,12 @@ object CHP3 {
       if (as.isEmpty) Nil
       else Cons(as.head, apply(as.tail: _*))
     }
+
+    def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+      as match {
+        case Nil => z
+        case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+      }
   }
 
   import List.sum
@@ -72,10 +97,92 @@ object CHP3 {
     }
   }
 
+  // ex3.6
+  def init[A](l: List[A]): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(_, Nil) => Nil
+      case Cons(x, xs) => Cons(x, init(xs))
+    }
+  }
+
+  // ex3.7
+  // 不会，会先展开，再运算，短路数为O(n)
+
+  // ex3.8，返回原列表
+  foldRight(List(1, 2, 3, 4), Nil: List[Int])(Cons(_, _))
+
+  // ex3.9
+  def length[A](as: List[A]): Int = {
+    foldRight(as, 0)((_, acc) => acc + 1)
+  }
+
+  // ex3.10
+  @tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = {
+    as match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
+  }
+
+  // ex3.11
+  def sum2(ints: List[Int]): Int = {
+    foldLeft(ints, 0)(_ + _)
+  }
+
+  def product2(ds: List[Double]): Double = {
+    foldLeft(ds, 1.0)(_ * _)
+  }
+
+  def length2[A](as: List[A]): Int = {
+    foldLeft(as, 0)((acc, _) => acc + 1)
+  }
+
+  // ex3.12
+  def reverse[A](as: List[A]): List[A] = {
+    foldLeft(as, Nil: List[A])((xs, x) => Cons(x, xs))
+  }
+
+  // ex3.14
+  def append[A](a1: List[A], a2: List[A]): List[A] = {
+    //foldLeft(reverse(a1), a2)((a2, x1) => Cons(x1, a2))
+    foldRight(a1, a2)((x1, a2) => Cons(x1, a2))
+  }
+
+  // ex3.16
+  def inc(lt: List[Int]): List[Int] = {
+    lt match {
+      case Nil => Nil
+      case Cons(x, xs) => Cons(1 + x, inc(xs))
+    }
+  }
+
+  // ex3.17
+  def stringy(lt: List[Double]): List[String] = {
+    lt match {
+      case Nil => Nil
+      case Cons(x, xs) => Cons(x.toString, stringy(xs))
+    }
+  }
+
+  // ex3.18
+  def map[A, B](as: List[A])(f: A => B): List[B] = {
+    as match {
+      case Nil => Nil
+      case Cons(x, xs) => Cons(f(x), map(xs)(f))
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     println(List("a", "b"))
     println(x)
     println(drop(List(1, 2, 3, 4, 5), 2))
     println(dropWhile(List(1, 2, 3, 4, 5), (x: Int) => x < 3))
+    println(init(List(1, 2, 3, 4, 5)))
+    println(foldRight(List(1, 2, 3, 4), Nil: List[Int])(Cons(_, _)))
+    println(reverse(List(1, 2, 3, 4)))
+    println(append(List(1, 2, 3), List(4, 5)))
+    println(inc(List(1, 2, 3)))
   }
 }
